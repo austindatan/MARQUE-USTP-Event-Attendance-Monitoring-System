7c3371daf97b4b05d4e723728a5e660b0f74a850
seyoon <austindatan@gmail.com>
2025-11-08 10:02:21 +0800

    Login & Splashscreen

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View, ImageBackground, Image } from "react-native";
+import React, { useEffect } from "react";
+import { useRouter } from "expo-router";
 
-export default function Index() {
+const Index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/login");
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <ImageBackground
+      source={require("../assets/images/marque/SplashScreen.png")}
+      style={[styles.background, {alignSelf: "center" }]}
+      resizeMode="cover"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View style={styles.container}>
+        <Image source={require("../assets/images/marque/MARQUE.png")} style={styles.logo} />
+      </View>
+    </ImageBackground>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  logo: {
+    width: 280,
+    height: 134,
+    resizeMode: "contain",
+    marginBottom: 6,
+  },
+});
+
+export default Index;
