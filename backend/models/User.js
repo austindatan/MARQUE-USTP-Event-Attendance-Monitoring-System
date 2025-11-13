cd29825a551f26fea97fb601924fbd7b84bc7b43
Vonzelle Puray <puray.vonzellefiona@gmail.com>
2025-11-13 17:02:16 +0800

    gi balik nakong user kay basig di maka sulod si sabrina

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
   lastname: { type: String, required: true },
   contact_number: { type: String },
   email: { type: String, required: true, unique: true },
-  role: { type: String, enum: ["Manager", "Committee",], required: true },
+  role: { type: String, enum: ["Manager", "Committee", "user"], required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
