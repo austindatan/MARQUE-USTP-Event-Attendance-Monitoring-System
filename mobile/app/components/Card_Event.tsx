d1f70fa6d53536dc421902f0ccec50180f80bb40
seyoon <austindatan@gmail.com>
2025-11-14 15:52:49 +0800

    responsive

--- mobile/app/components/Card_Event.tsx
+++ mobile/app/components/Card_Event.tsx
@@ -5,36 +5,38 @@ import styles from "../styles/components_eventcard";
 
 const EventCard = ({ image, title, orgLogo, organization, orgDate, dateDay, dateMonth, description, }) => {
   return (
-    <View style={styles.card}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={typeof image === "string" ? { uri: image } : image}
-          style={styles.eventPoster}
-        />
-
-        <View style={styles.dateTag}>
-          <Text style={styles.dateDay}>{dateDay}</Text>
-          <Text style={styles.dateMonth}>{dateMonth}</Text>
-        </View>
-      </View>
-
-      <View style={styles.details}>
-        <Text style={styles.eventTitle}>{title}</Text>
-
-        <View style={styles.orgDetails}>
+    <View style={styles.shadowWrapper}>
+      <View style={styles.card}>
+        <View style={styles.imageContainer}>
           <Image
-            source={typeof orgLogo === "string" ? { uri: orgLogo } : orgLogo}
-            style={styles.organizationLogo}
+            source={typeof image === "string" ? { uri: image } : image}
+            style={styles.eventPoster}
           />
-          <View style={styles.orgRow}>
-            <Text style={styles.orgText}>{organization}</Text>
-            <Text style={styles.subText}>{orgDate}</Text>
+
+          <View style={styles.dateTag}>
+            <Text style={styles.dateDay}>{dateDay}</Text>
+            <Text style={styles.dateMonth}>{dateMonth}</Text>
           </View>
         </View>
 
-        <Text style={styles.desc}>
-          {description}
-        </Text>
+        <View style={styles.details}>
+          <Text style={styles.eventTitle}>{title}</Text>
+
+          <View style={styles.orgDetails}>
+            <Image
+              source={typeof orgLogo === "string" ? { uri: orgLogo } : orgLogo}
+              style={styles.organizationLogo}
+            />
+            <View style={styles.orgRow}>
+              <Text style={styles.orgText}>{organization}</Text>
+              <Text style={styles.subText}>{orgDate}</Text>
+            </View>
+          </View>
+
+          <Text style={styles.desc}>
+            {description}
+          </Text>
+        </View>
       </View>
     </View>
   );
