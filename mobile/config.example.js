61f751db4afbf148d88970e87b06880ee759685e
seyoon <austindatan@gmail.com>
2026-02-21 23:04:42 +0800

    security: add .gitignore and example config templates, remove secrets from tracking

---
+++ mobile/config.example.js
@@ -0,0 +1,3 @@
+// Copy this file to config.js and fill in your local IP address
+// Run `ipconfig` (Windows) or `ifconfig` (Mac/Linux) to find your IP
+export const BASE_URL = "http://<YOUR_LOCAL_IP>:5000";
